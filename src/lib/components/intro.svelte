d7772748e03d362457cf6dea4c0d2a4480486308
Leo Nistor <leo.nistor@gmail.com>
2024-06-29 15:12:27 +0300

    cleanup styles, prepare for tailwind

--- src/lib/components/intro.svelte
+++ src/lib/components/intro.svelte
@@ -1,7 +1,1 @@
-<!-- intro
-        ----------------------------------------------- -->
-<section id="intro" class="s-intro target-section">
-  <div class="s-intro__bg"></div>
-  intro
-</section>
-<!-- end s-intro -->
+<h3>Intro</h3>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,6 @@
   import Intro from '$lib/components/intro.svelte'
 </script>
 
-<div id="page" class="s-pagewrap">
-  <Header />
-
-  <!-- # site-content
-    ================================================== -->
-  <section id="content" class="s-content">
-    <Intro />
-    <About />
-  </section>
-</div>
+<Header />
+<Intro />
+<About />
